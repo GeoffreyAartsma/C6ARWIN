62e718a9a6fbf77e6b9b9d0b1419b64770b9c395
Geoffrey Aartsma <GeoffreyAartsma@outlook.com>
2017-12-21 13:24:09 +0100

    Initial commit

---
+++ Assets/Fish/Scripts/FishSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishSpawner : MonoBehaviour {
+
+    [SerializeField]
+    public Rigidbody2D prefab;
+
+    // Use this for initialization
+    void Start()
+    {
+        Rigidbody2D clone;
+        clone = Instantiate(prefab, new Vector2(transform.position.x, Random.Range(-5.0f, 5.0f)), transform.rotation) as Rigidbody2D;
+        clone.velocity = new Vector2(-10f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Player/Scripts/CubeSchildpadController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSchildpadController : MonoBehaviour {
+    Rigidbody2D rb;
+
+    [SerializeField]
+    float maxSpeed;
+
+    [SerializeField]
+    float multiplyer;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        rb.constraints = RigidbodyConstraints2D.FreezePositionX |
+                         RigidbodyConstraints2D.FreezeRotation;
+        maxSpeed = 30f;
+        multiplyer = 8f;
+	}
+	
+	// FixedUpdate is called once per frame
+	void FixedUpdate () {
+        float moveVertical = Input.GetAxis("Vertical") * multiplyer;
+        Vector2 movement = new Vector2(0.0f, moveVertical);
+
+        rb.AddForce(movement);
+
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+
+        if (rb.position.y < -5f)
+        {
+            rb.MovePosition(new Vector2(rb.position.x, -5f));
+            rb.velocity = new Vector2(rb.velocity.x, -rb.velocity.y * 0.3f);
+        }
+        else if (rb.position.y > 5f)
+        {
+            rb.MovePosition(new Vector2(rb.position.x, 5f));
+            rb.velocity = new Vector2(rb.velocity.x, 0f);
+        }
+    }
+}
