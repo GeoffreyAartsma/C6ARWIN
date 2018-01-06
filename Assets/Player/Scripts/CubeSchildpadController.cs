f280efe14c60b8c387e84a153ffc3c08f2f5419e
Janneke Jonker <janneke.jonker@hotmail.com>
2018-01-06 16:51:06 +0100

    catching fish and count pointscores

--- Assets/Player/Scripts/CubeSchildpadController.cs
+++ Assets/Player/Scripts/CubeSchildpadController.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CubeSchildpadController : MonoBehaviour {
     Rigidbody2D rb;
+
+	public Text countText;
 
     [SerializeField]
     float maxSpeed;
 
     [SerializeField]
     float multiplyer;
+
+	private int count;
 
 	// Use this for initialization
 	void Start () {
@@ -18,6 +23,8 @@ public class CubeSchildpadController : MonoBehaviour {
                          RigidbodyConstraints2D.FreezeRotation;
         maxSpeed = 30f;
         multiplyer = 8f;
+		count = 0;
+		SetCountText ();
 	}
 	
 	// FixedUpdate is called once per frame
@@ -39,5 +46,20 @@ public class CubeSchildpadController : MonoBehaviour {
             rb.MovePosition(new Vector2(rb.position.x, 5f));
             rb.velocity = new Vector2(rb.velocity.x, 0f);
         }
-    }
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other) { 	
+		if (other.gameObject.CompareTag("PickUp"))
+		{
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+	}
 }
