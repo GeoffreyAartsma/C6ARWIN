a3c0309f7086ac98d658f5839e468a78ed349818
Janneke Jonker <janneke.jonker@hotmail.com>
2018-01-10 19:28:36 +0100

    git push
    Spawning fishes

---
+++ Assets/GameController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+	public GameObject fish;
+	public Vector3 spawnValues;
+	public int fishCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+
+	void Start ()
+	{
+		StartCoroutine (SpawnWaves ());
+	}
+
+	IEnumerator SpawnWaves ()
+	{
+		yield return new WaitForSeconds (startWait);
+		while (true)
+		{
+			for (int i = 0; i < fishCount; i++)
+			{
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (fish, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+		}
+	}
+}
