965f502de85c88834caba022bebeb37154087390
Unknown <GeoffreyAartsma@outlook.com>
2018-01-11 00:51:47 +0100

    FishSpawner

--- Assets/Fish/Scripts/FishSpawner.cs
+++ Assets/Fish/Scripts/FishSpawner.cs
@@ -6,18 +6,35 @@ public class FishSpawner : MonoBehaviour {
 
     [SerializeField]
     public Rigidbody2D prefab;
+    Rigidbody2D clone;
+
+    int wave = 1;
+
+    // TO DO: Add Wave System
+
 
     // Use this for initialization
     void Start()
     {
-        Rigidbody2D clone;
-        clone = Instantiate(prefab, new Vector2(transform.position.x, Random.Range(-5.0f, 5.0f)), transform.rotation) as Rigidbody2D;
-        clone.velocity = new Vector2(-10f, 0f);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (GameObject.FindGameObjectsWithTag("PickUp") == null) 
+        {
+            for (int i = 0; i < wave; i++)
+            {
+                clone = Instantiate(prefab, new Vector2(transform.position.x, Random.Range(-5.0f, 5.0f)), transform.rotation) as Rigidbody2D;
+                clone.velocity = new Vector2(-10f, 0f);
+            }
+            wave++;
+        }
+        else
+        {
+            Debug.Log(GameObject.FindGameObjectsWithTag("PickUp"));
+        }                
 
     }
 }
