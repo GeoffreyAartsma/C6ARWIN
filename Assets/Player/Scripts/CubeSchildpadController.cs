38f29c5b09045d2634fbec61ddbb5477b1436bea
danny <dannydorstijn1997@gmail.com>
2018-01-11 02:04:28 +0100

    Added game over controls

--- Assets/Player/Scripts/CubeSchildpadController.cs
+++ Assets/Player/Scripts/CubeSchildpadController.cs
@@ -6,18 +6,21 @@ using UnityEngine.UI;
 public class CubeSchildpadController : MonoBehaviour {
     Rigidbody2D rb;
 
-	public Text countText;
-
+    [SerializeField]
+	Text countText, gameOverText;
+    
     [SerializeField]
     float maxSpeed;
 
     [SerializeField]
     float multiplyer;
 
-	private int count;
+	int count;
+    
+    public bool Alive { get; set; }
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         rb = GetComponent<Rigidbody2D>();
         rb.constraints = RigidbodyConstraints2D.FreezePositionX |
                          RigidbodyConstraints2D.FreezeRotation;
@@ -29,25 +32,35 @@ public class CubeSchildpadController : MonoBehaviour {
 	
 	// FixedUpdate is called once per frame
 	void FixedUpdate () {
-        float moveVertical = Input.GetAxis("Vertical") * multiplyer;
-        Vector2 movement = new Vector2(0.0f, moveVertical);
-
-        rb.AddForce(movement);
-
-        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
-
-        if (rb.position.y < -5f)
+        if (Alive)
         {
-            rb.MovePosition(new Vector2(rb.position.x, -5f));
-            rb.velocity = new Vector2(rb.velocity.x, -rb.velocity.y * 0.3f);
+            float moveVertical = Input.GetAxis("Vertical") * multiplyer;
+            Vector2 movement = new Vector2(0.0f, moveVertical);
+
+            rb.AddForce(movement);
+
+            rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+
+            if (rb.position.y < -5f)
+            {
+                rb.MovePosition(new Vector2(rb.position.x, -5f));
+                rb.velocity = new Vector2(rb.velocity.x, -rb.velocity.y * 0.3f);
+            }
+            else if (rb.position.y > 5f)
+            {
+                rb.MovePosition(new Vector2(rb.position.x, 5f));
+                rb.velocity = new Vector2(rb.velocity.x, 0f);
+            }
         }
-        else if (rb.position.y > 5f)
+        else if (Input.GetKey(KeyCode.B))
         {
-            rb.MovePosition(new Vector2(rb.position.x, 5f));
-            rb.velocity = new Vector2(rb.velocity.x, 0f);
+            gameOverText.enabled = false;
+            rb.position = new Vector2(rb.position.x, 0);
+
+
+            Alive = true;
         }
 	}
-
 
 	void OnTriggerEnter2D(Collider2D other) { 	
 		if (other.gameObject.CompareTag("PickUp"))
@@ -56,10 +69,17 @@ public class CubeSchildpadController : MonoBehaviour {
 			count = count + 1;
 			SetCountText ();
 		}
+
+        if (other.gameObject.CompareTag("Obstacle"))
+        {
+            rb.velocity = Vector2.zero;
+            Alive = false;
+            gameOverText.enabled = true;
+        }
 	}
 
 	void SetCountText()
 	{
-		countText.text = "Count: " + count.ToString ();
+		countText.text = "Score: " + count.ToString ();
 	}
 }
